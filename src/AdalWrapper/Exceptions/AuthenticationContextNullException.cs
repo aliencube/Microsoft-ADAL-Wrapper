07ca41f5af9fad2b5ffc7850624ec085dd0e727f
Justin Yoo <justin.yoo@aliencube.com>
2016-06-13 23:45:16 +1000

    Initial source code commit

---
+++ src/AdalWrapper/Exceptions/AuthenticationContextNullException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace Aliencube.AdalWrapper.Exceptions
+{
+    /// <summary>
+    /// This represents the exception entity thrown when the <see cref="AuthenticationContext"/> instance is null.
+    /// </summary>
+    public class AuthenticationContextNullException : ApplicationException
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="AuthenticationContextNullException"/> class.
+        /// </summary>
+        public AuthenticationContextNullException()
+            : this("AuthenticationContext is null")
+        {
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="AuthenticationContextNullException"/> class.
+        /// </summary>
+        /// <param name="message">A message that describes the error. </param>
+        public AuthenticationContextNullException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="AuthenticationContextNullException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception. </param>
+        /// <param name="innerException">The exception that is the cause of the current exception. If the <paramref name="innerException" /> parameter is not a null reference, the current exception is raised in a catch block that handles the inner exception. </param>
+        public AuthenticationContextNullException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="AuthenticationContextNullException"/> class.
+        /// </summary>
+        /// <param name="info">The object that holds the serialized object data. </param>
+        /// <param name="context">The contextual information about the source or destination. </param>
+        protected AuthenticationContextNullException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
